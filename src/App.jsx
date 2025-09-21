8befb6855855adeb933d65c2d9178ca02eaa0ba0
Aditya <anupama12sg@gmail.com>
2025-09-21 23:25:40 +0530

    installed tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-
+      <h1 class="text-3xl font-bold font-mono text-amber-400">
+        Hello world!
+      </h1>
     </div>
   )
 }
