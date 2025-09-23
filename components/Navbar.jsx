428ba29ffab2a0405bc3812f4db0efb8a110fefc
Aditya <anupama12sg@gmail.com>
2025-09-23 22:19:28 +0530

    chore: imported NavLink and created a ul

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import assets from '../src/assets/frontend_assets/logo.png'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             <img src={assets} className='w-36' alt="" />
+            <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+
+            </ul>
+
         </div>
     )
 }
