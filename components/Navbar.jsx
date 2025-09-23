0eb409e0e81a1e4bb189d57c2de9b2c50f227071
Aditya <anupama12sg@gmail.com>
2025-09-23 20:37:12 +0530

    updated the title of the webpage

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import logo from '../src/assets/logo.png'
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='flex items-center justify-between py-5 font-medium'>
+            <img src={logo} className='w-36' alt="" />
 
         </div>
     )
