31aba407fc8ba891df289069fc5bd6f50a068c6f
Aditya <anupama12sg@gmail.com>
2025-09-24 22:12:35 +0530

    changed the p tags and added respective navlinks

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,8 +8,24 @@ const Navbar = () => {
             <img src={assets} className='w-36' alt="" />
 
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+
                 <NavLink to="/" className="flex flex-col items-center gap-1">
-                    <p>Home</p>
+                    <p>HOME</p>
+                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+                </NavLink>
+
+                <NavLink to="/collection" className="flex flex-col items-center gap-1">
+                    <p>COLLECTION</p>
+                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+                </NavLink>
+
+                <NavLink to="/about" className="flex flex-col items-center gap-1">
+                    <p>ABOUT</p>
+                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+                </NavLink>
+
+                <NavLink to="/contact" className="flex flex-col items-center gap-1">
+                    <p>CONTACT US</p>
                     <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
                 </NavLink>
             </ul>
