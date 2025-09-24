62608dc2fcb380c9891e25f84635bc535ccdf25c
Aditya <anupama12sg@gmail.com>
2025-09-24 23:04:38 +0530

    formatting code

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,6 +7,7 @@ import profile_icon from '../src/assets/frontend_assets/profile_icon.png'
 const Navbar = () => {
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
+
             <img src={assets} className='w-36' alt="" />
 
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
@@ -37,18 +38,18 @@ const Navbar = () => {
 
                 <div className='group relative'>
                     <img src={profile_icon} className='w-5 cursor-pointer' alt="" />
+
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
                             <p className='cursor-pointer hover:text-black'>Orders</p>
                             <p className='cursor-pointer hover:text-black'>Logout</p>
                         </div>
-
                     </div>
                 </div>
-
             </div>
-
         </div>
     )
 }
