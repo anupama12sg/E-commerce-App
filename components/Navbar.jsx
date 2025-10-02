9434c977efe15499b81d9872d4455baa4f09eaf9
Aditya <anupama12sg@gmail.com>
2025-10-02 15:50:48 +0530

    sidebar menu for small screen working properly

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -72,10 +72,10 @@ const Navbar = () => {
                         <p>Back</p>
                     </div>
 
-                    <NavLink className='py-2 pl-6 border' to='/'>HOME</NavLink>
-                    <NavLink className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
-                    <NavLink className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
-                    <NavLink className='py-2 pl-6 border' to='/contact'>CONTACT US</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                    <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT US</NavLink>
 
                 </div>
             </div>
