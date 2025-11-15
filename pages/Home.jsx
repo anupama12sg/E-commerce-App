f89e77c0d2b9dafe504db92e1003409852e9dc68
Aditya <anupama12sg@gmail.com>
2025-11-15 18:40:42 +0530

    rendered Hero component in the Home Page, made Hero Left section

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Route, Router } from 'react-router-dom'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
-      
+      <Hero />
     </div>
   )
 }
