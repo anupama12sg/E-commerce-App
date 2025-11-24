e3cb5251eff9c6576ffdbef2d1b3a2b54ec60a3f
Aditya <anupama12sg@gmail.com>
2025-11-24 21:14:29 +0530

    chore:rendered LatestCollection in Home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Router } from 'react-router-dom'
 import Hero from '../components/Hero'
+import LatestCollections from '../components/LatestCollections'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <LatestCollections />
     </div>
   )
 }
